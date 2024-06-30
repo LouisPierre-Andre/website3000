dd90ec8ecf49b69b001e7ebfa33b4d9884b11e10
unknown <Louis Pierre Andre>
2024-06-30 18:38:35 -0400

    Description de la modification

--- src/App.js
+++ src/App.js
@@ -1,45 +1,49 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import matter from 'gray-matter';
 import './App.css';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      // Utilisation de require.context pour importer les fichiers dynamiquement
+      const context = require.context('../posts', false, /\.md$/);
+      const postFiles = context.keys().map(context);
+
+      const postsData = await Promise.all(
+        postFiles.map(async (file) => {
+          const response = await fetch(file);
+          const text = await response.text();
+          const { data, content } = matter(text);
+          return { ...data, content };
+        })
+      );
+
+      setPosts(postsData);
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>THis is Website 3000</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Blog Posts</h1>
       </header>
+      <main>
+        {posts.map((post, index) => (
+          <article key={index}>
+            <h2>{post.title}</h2>
+            <p>{typeof post.date === 'string' ? post.date : post.date.toString()}</p>
+            {/* <img src={post.thumbnail} alt={post.title} /> */}
+            <ReactMarkdown>{post.content}</ReactMarkdown>
+          </article>
+        ))}
+      </main>
     </div>
   );
 }
 
 export default App;
-
-
-
-// git init
-// git add .
-// git commit -m "Initial commit"
-// git remote add origin https://github.com/LouisPierre-Andre/netlify2.git
-// git branch - M main
-// git push origin main
-
-
-
-
-// git add .
-// git commit -m "Initial commit"
-// git push origin main
-
-
-// git add public / admin / index.html public / admin / config.yml
-// git commit -m "Add Netlify CMS configuration"
-// git push origin main
-
